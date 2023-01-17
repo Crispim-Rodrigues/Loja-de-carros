2fbdb07ecb3c4e76dd38add4082212db36367dab
unknown <crispim.rodrigues.oficial@gmail.com>
2023-01-17 10:14:25 -0300

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,39 @@
 import { Link } from "react-router-dom";
-
+import '../App.css';
+import logo from '../images/logo.jpg';
+import { useRef, useState } from "react";
 function Navbar() {
+    const home = useRef(null);
+    const venda = useRef(null);
+    const sobre = useRef(null);
+    const contato = useRef(null);
+    const [reset, setReset] = useState(home);
+    
+    function Page(params) {
+        reset.current.style.color = "black";
+        params.current.style.color = "#ee4444";
+        setTimeout(()=>setReset(params),1)
+    }
 
     return(
         <header>
-            <div className="50w">
-
-            </div>
-            <div className="50w">
-                <nav>
-                    <ul>
-                        <Link to="/">Home</Link>
-                        <Link to="/Venda">Venda</Link>
-                        <Link to="/Sobre">Sobre</Link>
-                        <Link to="/Contato">Contato</Link>
-                    </ul>
-                </nav>
-            </div>
+            <div className="center">
+                <div className="w50">
+                    <div className="box_image">
+                        <img src={logo} alt={logo.name} draggable="false"/>
+                    </div>
+                </div>
+                <div className="w50">
+                    <nav>
+                        <ul>
+                            <li><Link ref={home} onClick={()=>Page(home)} to="/">Home</Link></li>
+                            <li><Link ref={venda} onClick={()=>Page(venda)} to="/Venda">Venda</Link></li>
+                            <li><Link ref={sobre} onClick={()=>Page(sobre)} to="/Sobre">Sobre</Link></li>
+                            <li><Link ref={contato} onClick={()=>Page(contato)} to="/Contato">Contato</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>    
         </header>
     )
 }
--- src/pages/Venda.js
+++ src/pages/Venda.js
@@ -1,5 +1,4 @@
 
-
 function Venda() 
 {
     return(
