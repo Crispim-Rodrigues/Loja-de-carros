cf02e34449682a4f71b9c31c7ed59bac433b464a
unknown <crispim.rodrigues.oficial@gmail.com>
2023-01-19 23:11:56 -0300

    Destaques

--- src/components/Home-Components/Destaques.js
+++ src/components/Home-Components/Destaques.js
@@ -1,15 +1,31 @@
 import "../../App.css";
-
+import carro from '../../images/carro1.jpg'
 function Destaques() {
+    const car = []
+
+    for(var i = 0; i < 3; i++){
+        car.push(
+    
+            <div key={i} className="singlecar">
+                <img src={carro} alt=""/>
+                <h4>Lorem ipsum</h4>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing vitae dolorem voluptas a </p>
+                <a href="/#">Mais detalhes</a>
+            </div>
+
+        )     
+    }
+
+    console.log(car[0]);
     return(
         <section className="destaques_main">
             <div className="destaques_title">
-            <span></span>
-            <h1>VEÍCULOS EM DESTAQUE</h1>
-            <span></span>
+            <div></div>
+            <h2>VEÍCULOS EM DESTAQUE</h2>
+            <div></div>
             </div>
             <div className="destaques_boxes">
-
+                {car}
             </div>
         </section>
     )
